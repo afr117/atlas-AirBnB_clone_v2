9efc7bd586a234fa005a7c183cc9fc270dc35e94
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-11 20:00:05 +0000

    lines too long

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,8 +4,10 @@ This module starts a Flask web application that listens on 0.0.0.0, port 5000.
 Routes:
 /: display "Hello HBNB!"
 /hbnb: display "HBNB"
-/c/<text>: display "C " followed by the value of the text variable (replace underscore _ symbols with a space)
-/python/<text>: display "Python " followed by the value of the text variable (replace underscore _ symbols with a space). The default value of text is "is cool"
+/c/<text>: display "C " followed by the value of the text variable (replace
+underscore _ symbols with a space)
+/python/<text>: display "Python "
+followed by the value of the text variable (replace underscore _ symbols with a space). The default value of text is "is cool"
 """
 
 from flask import Flask
@@ -40,4 +42,3 @@ def python_text(text):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
