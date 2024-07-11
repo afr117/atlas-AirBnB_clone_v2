47a846b255245ba59a560889b3a41a8d8e9cfc04
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-11 19:32:45 +0000

    test

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,7 +4,8 @@ This module starts a Flask web application that listens on 0.0.0.0, port 5000.
 Routes:
 /: display "Hello HBNB!"
 /hbnb: display "HBNB"
-/c/<text>: display "C " followed by the value of the text variable (replace underscore _ symbols with a space)
+/c/<text>: display "C " followed by the value of the text variable (replace 
+underscore _ symbols with a space)
 """
 
 from flask import Flask
