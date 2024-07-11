6ccbfb292d747317f4a7d716aaaa9be371268f8e
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-11 20:27:29 +0000

    t5 pep8 validated

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -12,8 +12,7 @@ followed by the value of the text variable
 The default value of text is "is cool"
 /number/<n>: display "n is a number"
 only if n is an integer
-/number_template/<n>: display a HTML page only
-if n is an integer:
+/number_template/<n>: display a HTML page only if n is an integer:
     H1 tag: "Number: n" inside the tag BODY
 """
 
