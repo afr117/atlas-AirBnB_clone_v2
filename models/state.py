4cd0a24e34f36a88662955712ff2ae48c2ebd4ed
afr117 <alfredo.figueroa07@yahoo.com>
2024-07-19 04:00:32 +0000

    t7 load

---
+++ models/state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" holds class State"""
+
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class State(BaseModel, Base):
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+    cites = []
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """
+            cities getter for instances linked to a state
+            """
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
