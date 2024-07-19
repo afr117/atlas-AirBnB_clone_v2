7200a49c8595a213e41d987547c75d8a158a9c46
afr117 <54492982+afr117@users.noreply.github.com>
2024-07-18 23:47:49 -0400

    Update 5-number_template.py
    test

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,65 +1,53 @@
 #!/usr/bin/python3
 
-"""
-Hello World! script that has routing for the following:
-    '/',
-    '/hbnb'
-    '/c/<usertext>'
-    '/python/<usertext>'
-"""
+""" 5. Add fifth view func that displays HTML page if n is int """
 
-from flask import Flask, render_template
-from markupsafe import escape
+from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
 def hello():
-    """
-    Hello, HBNB!
-    """
-    return "Hello HBNB!"
+    """ Return other text. """
+    return 'HBNB'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    HBNB
-    """
-    return "HBNB"
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """
-    C {}
-    """
-    return "C {}".format(escape(text.replace("_", " ")))
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text="is cool"):
-    """
-    'Python {}' (has default)
-    """
-    return "Python {}".format(escape(text.replace("_", " ")))
+@app.route('/number/<int:n>')
+def number_text(n):
+    """ replace with int only if given int. """
+    n = str(n)
+    return '{} is a number'.format(n)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n=0):
-    """
-    '{n} is a number'
-    """
-    return "{} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """
-    'Number: {}'
-    """
+@app.route('/number_template/<int:n>')
+def html_num(n):
+    """ display html if n is int. """
+    n = str(n)
     return render_template('5-number.html', n=n)
 
 
